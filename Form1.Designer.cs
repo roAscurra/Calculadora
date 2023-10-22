461b20a9cfcd1e91d5499d878b238986433f4b2d
roAscurra <rominaascurra4887@gmail.com>
2023-10-22 15:13:45 -0300

    Botones de abrir y cerrar parentesis, signo, cero, borrar y borrar todo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,9 +43,26 @@ partial class Form1
         this.btnSumar = new System.Windows.Forms.Button();
         this.btnResultado = new System.Windows.Forms.Button();
         this.btnPunto = new System.Windows.Forms.Button();
+        this.btnCero = new System.Windows.Forms.Button();
+        this.btnParentesis1 = new System.Windows.Forms.Button();
+        this.btnParentesis2 = new System.Windows.Forms.Button();
+        this.btnBorrar = new System.Windows.Forms.Button();
+        this.btnBorrarTodo = new System.Windows.Forms.Button();
+        this.btnSigno = new System.Windows.Forms.Button();
 
-        
         this.SuspendLayout();
+
+        // 
+        // btnCero
+        // 
+        this.btnCero.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnCero.Location = new System.Drawing.Point(70, 310);
+        this.btnCero.Name = "btnCero";
+        this.btnCero.Size = new System.Drawing.Size(45, 45);
+        this.btnCero.TabIndex = 21;
+        this.btnCero.Text = "0";
+        this.btnCero.UseVisualStyleBackColor = true;
+        //this.btnCero.Click += new System.EventHandler(this.agregarNumero);
 
         // btnUno
         // 
@@ -126,7 +143,7 @@ partial class Form1
         this.btnSiete.UseVisualStyleBackColor = true;
         //this.btnSiete.Click += new System.EventHandler(this.agregarNumero);
         // 
-        
+
         // btnOcho
         // 
         this.btnOcho.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -137,7 +154,7 @@ partial class Form1
         this.btnOcho.Text = "8";
         this.btnOcho.UseVisualStyleBackColor = true;
         //this.btnOcho.Click += new System.EventHandler(this.agregarNumero);
-    
+
         // btnNueve
         // 
         this.btnNueve.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -152,7 +169,7 @@ partial class Form1
         // btnMultiplicar
         // 
         this.btnMultiplicar.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-        this.btnMultiplicar.Location = new System.Drawing.Point(271, 131);
+        this.btnMultiplicar.Location = new System.Drawing.Point(271, 190);
         this.btnMultiplicar.Name = "btnMultiplicar";
         this.btnMultiplicar.Size = new System.Drawing.Size(45, 45);
         this.btnMultiplicar.TabIndex = 16;
@@ -165,12 +182,12 @@ partial class Form1
         // btnDividir
         // 
         this.btnDividir.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-        this.btnDividir.Location = new System.Drawing.Point(214, 131);
+        this.btnDividir.Location = new System.Drawing.Point(214, 190);
         this.btnDividir.Name = "btnDividir";
         this.btnDividir.Size = new System.Drawing.Size(45, 45);
         this.btnDividir.TabIndex = 15;
-        this.btnDividir.Tag = "∕";
-        this.btnDividir.Text = "∕";
+        this.btnDividir.Tag = "/";
+        this.btnDividir.Text = "/";
         this.btnDividir.UseVisualStyleBackColor = true;
         this.btnDividir.Click += new System.EventHandler(this.clickOperador);
         // 
@@ -178,7 +195,7 @@ partial class Form1
         // btnRestar
         // 
         this.btnRestar.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-        this.btnRestar.Location = new System.Drawing.Point(271, 190);
+        this.btnRestar.Location = new System.Drawing.Point(271, 249);
         this.btnRestar.Name = "btnRestar";
         this.btnRestar.Size = new System.Drawing.Size(45, 45);
         this.btnRestar.TabIndex = 18;
@@ -188,21 +205,21 @@ partial class Form1
         this.btnRestar.Click += new System.EventHandler(this.clickOperador);
         // 
         // btnSumar
-            // 
-            this.btnSumar.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSumar.Location = new System.Drawing.Point(214, 190);
-            this.btnSumar.Name = "btnSumar";
-            this.btnSumar.Size = new System.Drawing.Size(45, 45);
-            this.btnSumar.TabIndex = 17;
-            this.btnSumar.Tag = "+";
-            this.btnSumar.Text = "+";
-            this.btnSumar.UseVisualStyleBackColor = true;
-            this.btnSumar.Click += new System.EventHandler(this.clickOperador);
-            // 
+        // 
+        this.btnSumar.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnSumar.Location = new System.Drawing.Point(214, 249);
+        this.btnSumar.Name = "btnSumar";
+        this.btnSumar.Size = new System.Drawing.Size(45, 45);
+        this.btnSumar.TabIndex = 17;
+        this.btnSumar.Tag = "+";
+        this.btnSumar.Text = "+";
+        this.btnSumar.UseVisualStyleBackColor = true;
+        this.btnSumar.Click += new System.EventHandler(this.clickOperador);
+        // 
         // btnResultado
         // 
         this.btnResultado.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-        this.btnResultado.Location = new System.Drawing.Point(214, 249);
+        this.btnResultado.Location = new System.Drawing.Point(214, 310);
         this.btnResultado.Name = "btnResultado";
         this.btnResultado.Size = new System.Drawing.Size(102, 45);
         this.btnResultado.TabIndex = 19;
@@ -221,14 +238,72 @@ partial class Form1
         this.btnPunto.Text = ".";
         this.btnPunto.UseVisualStyleBackColor = true;
         this.btnPunto.Click += new System.EventHandler(this.btnPunto_Click);
+        //
         // 
+        // btnParentesis1
+        // 
+        this.btnParentesis1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnParentesis1.Location = new System.Drawing.Point(271, 131);
+        this.btnParentesis1.Name = "btnParentesis1";
+        this.btnParentesis1.Size = new System.Drawing.Size(45, 45);
+        this.btnParentesis1.TabIndex = 14;
+        this.btnParentesis1.Tag = ")";
+        this.btnParentesis1.Text = ")";
+        this.btnParentesis1.UseVisualStyleBackColor = true;
+        //this.btnParentesis1.Click += new System.EventHandler(this.clickOperador);
+        // 
+        // btnParentesis2
+        // 
+        this.btnParentesis2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnParentesis2.Location = new System.Drawing.Point(214, 131);
+        this.btnParentesis2.Name = "btnParentesis2";
+        this.btnParentesis2.Size = new System.Drawing.Size(45, 45);
+        this.btnParentesis2.TabIndex = 13;
+        this.btnParentesis2.Tag = "(";
+        this.btnParentesis2.Text = "(";
+        this.btnParentesis2.UseVisualStyleBackColor = true;
+        //this.btnParentesis2.Click += new System.EventHandler(this.clickOperador); 
+        // 
+        // btnBorrar
+        // 
+        this.btnBorrar.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnBorrar.Location = new System.Drawing.Point(214, 71);
+        this.btnBorrar.Name = "btnBorrar";
+        this.btnBorrar.Size = new System.Drawing.Size(100, 45);
+        this.btnBorrar.TabIndex = 10;
+        this.btnBorrar.Text = "Borrar";
+        this.btnBorrar.UseVisualStyleBackColor = true;
+        //this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+        // 
+        // btnBorrarTodo
+        // 
+        this.btnBorrarTodo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnBorrarTodo.Location = new System.Drawing.Point(13, 71);
+        this.btnBorrarTodo.Name = "btnBorrarTodo";
+        this.btnBorrarTodo.Size = new System.Drawing.Size(150, 45);
+        this.btnBorrarTodo.TabIndex = 11;
+        this.btnBorrarTodo.Text = "Borrar Todo";
+        this.btnBorrarTodo.UseVisualStyleBackColor = true;
+        //this.btnBorrarTodo.Click += new System.EventHandler(this.btnBorrarTodo_Click);
+        // 
+        // btnSigno
+        // 
+        this.btnSigno.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+        this.btnSigno.Location = new System.Drawing.Point(13, 310);
+        this.btnSigno.Name = "btnSigno";
+        this.btnSigno.Size = new System.Drawing.Size(45, 45);
+        this.btnSigno.TabIndex = 20;
+        this.btnSigno.Text = "+/-";
+        this.btnSigno.UseVisualStyleBackColor = true;
+        //this.btnSigno.Click += new System.EventHandler(this.btnSigno_Click);
+
 
         // Form1
         // 
         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(331, 365);
-        
+
         this.Controls.Add(this.btnCuatro);
         this.Controls.Add(this.btnTres);
         this.Controls.Add(this.btnDos);
@@ -244,6 +319,15 @@ partial class Form1
         this.Controls.Add(this.btnResultado);
         this.Controls.Add(this.btnRestar);
         this.Controls.Add(this.btnPunto);
+        this.Controls.Add(this.btnCero);
+        this.Controls.Add(this.btnParentesis1);
+        this.Controls.Add(this.btnParentesis2);
+        this.Controls.Add(this.btnBorrar);
+        this.Controls.Add(this.btnBorrarTodo);
+        this.Controls.Add(this.btnSigno);
+
+
+
         this.MaximizeBox = false;
         this.Name = "Form1";
         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -253,6 +337,7 @@ partial class Form1
     }
 
     #endregion
+    private System.Windows.Forms.Button btnCero;
     private System.Windows.Forms.Button btnUno;
     private System.Windows.Forms.Button btnDos;
     private System.Windows.Forms.Button btnTres;
@@ -268,7 +353,14 @@ partial class Form1
     private System.Windows.Forms.Button btnSumar;
     private System.Windows.Forms.Button btnResultado;
     private System.Windows.Forms.Button btnPunto;
+    private System.Windows.Forms.Button btnParentesis1;
+
+    private System.Windows.Forms.Button btnParentesis2;
+    private System.Windows.Forms.Button btnBorrar;
+    private System.Windows.Forms.Button btnBorrarTodo;
+    private System.Windows.Forms.Button btnSigno;
+
     private System.Windows.Forms.TextBox txtResultado;
 
-        
+
 }
