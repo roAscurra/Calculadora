b1988f98a721e3bcc95c12f907b971e4e2000638
FlorGubiotti <florgubiotti@gmail.com>
2023-11-01 19:59:07 -0300

    Error paréntesis sin operador delante

--- Form1.cs
+++ Form1.cs
@@ -1,161 +1,172 @@
-namespace Calculadora{
-
-public partial class Form1 : Form
+namespace Calculadora
 {
 
-    private string currentExpression = "";
-
-    public Form1()
+    public partial class Form1 : Form
     {
-        // Configurar la configuración regional de Windows para usar el punto (.) como separador decimal
-        var cultureInfo = new System.Globalization.CultureInfo("en-US");
-        cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
-        Thread.CurrentThread.CurrentCulture = cultureInfo;
-        Thread.CurrentThread.CurrentUICulture = cultureInfo;
-        InitializeComponent();
-    }
-    private void agregarNumero(object sender, EventArgs e)
-    {
-        Button button = (Button)sender;
-        currentExpression += button.Text;
-        txtResultado.Text = currentExpression.Replace("0-", "-");
-    }
 
-    private void clickOperador(object sender, EventArgs e)
-    {
-        Button button = (Button)sender;
+        private string currentExpression = "";
 
-        if (button.Text == "-" && string.IsNullOrEmpty(currentExpression))
+        public Form1()
         {
-            currentExpression += "0" + button.Text; // Agrega un 0 antes del '-' para manejar números negativos.
+            // Configurar la configuración regional de Windows para usar el punto (.) como separador decimal
+            var cultureInfo = new System.Globalization.CultureInfo("en-US");
+            cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            InitializeComponent();
         }
-        else if (IsOperator(button.Text[0]) && !string.IsNullOrEmpty(currentExpression) && !IsOperator(currentExpression[currentExpression.Length - 1]) && currentExpression[currentExpression.Length - 1] != '(')
+        private void agregarNumero(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
             currentExpression += button.Text;
-        }
-        else if (button.Text == "(" || button.Text == ")" || button.Text != "-" && !string.IsNullOrEmpty(currentExpression) && !IsOperator(currentExpression[currentExpression.Length - 1]))
-        {
-            currentExpression += button.Text;
-        }
-
-        txtResultado.Text = currentExpression.Replace("0-", "-");
-    }
-    private bool IsOperator(char c)
-    {
-        return (c == '+' || c == '-' || c == 'x' || c == '/');
-    }
-
-    private int Precedence(char op)
-    {
-        if (op == '+' || op == '-')
-            return 1;
-        if (op == 'x' || op == '/')
-            return 2;
-        return 0;
-    }
-
-    private void ComputeTopOperation(Stack<double> numberStack, Stack<char> operatorStack)
-    {
-        char op = operatorStack.Pop();
-        double b = numberStack.Pop();
-        double a = numberStack.Pop();
-
-        double result = 0;
-        switch (op)
-        {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case 'x':
-                result = a * b;
-                break;
-            case '/':
-                if (b == 0)
-                {
-                    MessageBox.Show("No es posible dividir entre cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    throw new DivideByZeroException();
-                }
-                result = a / b;
-                break;
-        }
-
-        numberStack.Push(result);
-    }
-    private void btnBorrar_Click(object sender, EventArgs e)
-    {
-        if (currentExpression.Length > 0)
-        {
-            currentExpression = currentExpression.Substring(0, currentExpression.Length - 1);
             txtResultado.Text = currentExpression.Replace("0-", "-");
         }
-    }
-    private void btnBorrarTodo_Click(object sender, EventArgs e)
-    {
-        currentExpression = "";
-        txtResultado.Text = "0";
-    }
-    private void btnResultado_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            double result = EvaluateExpression(currentExpression);
-            txtResultado.Text = result.ToString();
-            currentExpression = result.ToString();
-        }
-        catch (Exception)
-        {
-            MessageBox.Show("El formato usado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            // Si la expresión no es válida, no hagas nada y simplemente regresa sin mostrar un mensaje de error.
-            return;
-        }
-    }
-    private double ParseDouble(string s)
-    {
-        double result = 0.0;
-        double fraction = 0.1;
-        bool isFraction = false;
-        bool isNegative = false;
 
-        foreach (char c in s)
+        private void clickOperador(object sender, EventArgs e)
         {
-            if (c == '-')
+            Button button = (Button)sender;
+
+            if (button.Text == "-" && string.IsNullOrEmpty(currentExpression))
             {
-                isNegative = true;
+                currentExpression += "0" + button.Text; // Agrega un 0 antes del '-' para manejar números negativos.
             }
-            else if (c == '.')
+            else if (IsOperator(button.Text[0]) && !string.IsNullOrEmpty(currentExpression) && !IsOperator(currentExpression[currentExpression.Length - 1]) && currentExpression[currentExpression.Length - 1] != '(')
             {
-                isFraction = true;
+                currentExpression += button.Text;
             }
-            else
+            else if (button.Text == "(")
             {
-                int digit = c - '0';
-                if (isFraction)
+                if (!string.IsNullOrEmpty(currentExpression) && (char.IsDigit(currentExpression[currentExpression.Length - 1]) || currentExpression[currentExpression.Length - 1] == ')'))
                 {
-                    result += digit * fraction;
-                    fraction *= 0.1;
+                    MessageBox.Show("Formato inválido: Debe colocar un operador antes.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Muestra un mensaje de error si se intenta agregar un paréntesis de apertura después de un número o un paréntesis de cierre.
+                }
+                currentExpression += button.Text;
+            }
+            else if (button.Text != "-" && !string.IsNullOrEmpty(currentExpression) && !IsOperator(currentExpression[currentExpression.Length - 1]))
+            {
+                currentExpression += button.Text;
+            }
+
+            txtResultado.Text = currentExpression.Replace("0-", "-");
+        }
+
+        private bool IsOperator(char c)
+        {
+            return (c == '+' || c == '-' || c == 'x' || c == '/');
+        }
+
+        private int Precedence(char op)
+        {
+            if (op == '+' || op == '-')
+                return 1;
+            if (op == 'x' || op == '/')
+                return 2;
+            return 0;
+        }
+
+        private void ComputeTopOperation(Stack<double> numberStack, Stack<char> operatorStack)
+        {
+            char op = operatorStack.Pop();
+            double b = numberStack.Pop();
+            double a = numberStack.Pop();
+
+            double result = 0;
+            switch (op)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case 'x':
+                    result = a * b;
+                    break;
+                case '/':
+                    if (b == 0)
+                    {
+                        MessageBox.Show("No es posible dividir entre cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        throw new DivideByZeroException();
+                    }
+                    result = a / b;
+                    break;
+            }
+
+            numberStack.Push(result);
+        }
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (currentExpression.Length > 0)
+            {
+                currentExpression = currentExpression.Substring(0, currentExpression.Length - 1);
+                txtResultado.Text = currentExpression.Replace("0-", "-");
+            }
+        }
+        private void btnBorrarTodo_Click(object sender, EventArgs e)
+        {
+            currentExpression = "";
+            txtResultado.Text = "0";
+        }
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = EvaluateExpression(currentExpression);
+                txtResultado.Text = result.ToString();
+                currentExpression = result.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El formato usado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Si la expresión no es válida, no hagas nada y simplemente regresa sin mostrar un mensaje de error.
+                return;
+            }
+        }
+        private double ParseDouble(string s)
+        {
+            double result = 0.0;
+            double fraction = 0.1;
+            bool isFraction = false;
+            bool isNegative = false;
+
+            foreach (char c in s)
+            {
+                if (c == '-')
+                {
+                    isNegative = true;
+                }
+                else if (c == '.')
+                {
+                    isFraction = true;
                 }
                 else
                 {
-                    result = result * 10 + digit;
+                    int digit = c - '0';
+                    if (isFraction)
+                    {
+                        result += digit * fraction;
+                        fraction *= 0.1;
+                    }
+                    else
+                    {
+                        result = result * 10 + digit;
+                    }
                 }
             }
-        }
 
-        if (isNegative)
-        {
-            result = -result;
-        }
+            if (isNegative)
+            {
+                result = -result;
+            }
 
-        return result;
-    }
+            return result;
+        }
         private bool IsDigit(char c)
         {
             return (c >= '0' && c <= '9') || c == '.';
         }
-           private double EvaluateExpression(string expression)
+        private double EvaluateExpression(string expression)
         {
             var numberStack = new Stack<double>();
             var operatorStack = new Stack<char>();
@@ -226,29 +237,29 @@ public partial class Form1 : Form
                 throw new Exception("Expresión inválida");
             }
         }
-    private void btnPunto_Click(object sender, EventArgs e)
-    {
-        if (currentExpression.Length == 0 || !currentExpression.EndsWith(".") && !currentExpression.Contains("."))
+        private void btnPunto_Click(object sender, EventArgs e)
         {
-            //Comprueba si la expresión actual está vacía o no termina con un punto decimal
-            currentExpression += ".";
-            txtResultado.Text = currentExpression;
+            if (currentExpression.Length == 0 || !currentExpression.EndsWith(".") && !currentExpression.Contains("."))
+            {
+                //Comprueba si la expresión actual está vacía o no termina con un punto decimal
+                currentExpression += ".";
+                txtResultado.Text = currentExpression;
+            }
+        }
+        private void btnSigno_Click(object sender, EventArgs e)
+        {
+            if (currentExpression.Length > 0)
+            {
+                if (currentExpression[0] == '-')
+                {
+                    currentExpression = currentExpression.Substring(1);
+                }
+                else
+                {
+                    currentExpression = "-" + currentExpression;
+                }
+                txtResultado.Text = currentExpression.Replace("0-", "-");
+            }
         }
     }
-    private void btnSigno_Click(object sender, EventArgs e)
-    {
-        if (currentExpression.Length > 0)
-        {
-            if (currentExpression[0] == '-')
-            {
-                currentExpression = currentExpression.Substring(1);
-            }
-            else
-            {
-                currentExpression = "-" + currentExpression;
-            }
-            txtResultado.Text = currentExpression.Replace("0-", "-");
-        }
-    }
-}
 }
